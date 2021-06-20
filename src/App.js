e2c488f6b64be659ec7e058646ba88edcc8152f7
Ümmühan Keleş <ummuhankeles4@gmail.com>
2021-06-20 19:26:03 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react';
+import Info from "./component/info/Info";
+import Calculate from './component/calculate/Calculate';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container">
+      <Info/>
+      <Calculate/>
     </div>
   );
 }
